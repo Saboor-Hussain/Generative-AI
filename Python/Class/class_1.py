1cac1b5eee46ebad6f3dc29b5c643a879c3c2936
SaboorHussain <hussainsaboor456@gmail.com>
2023-10-19 20:10:15 +0500

    first commit

---
+++ Python/Class/class_1.py
@@ -0,0 +1,13 @@
+
+# Old Syntax of Python
+
+# name = "Saboor Hussain"
+# name = 100
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) # physical address
+
+
+# Typed Based Syntax of Python
+name: str = "Saboor Hussain"
+print(name)
